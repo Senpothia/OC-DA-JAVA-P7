e9af5cd038b9d70ea0b4bb244e81f59e8a42a6f5
Senpothia <kintelalpz@gmail.com>
2020-04-14 11:07:43 +0200

    init

--- src/main/java/com/formation/projet7/model/Emprunt.java
+++ src/main/java/com/formation/projet7/model/Emprunt.java
@@ -11,5 +11,79 @@ public class Emprunt {
 	private Date fin;
 	private boolean prolongation; 
 	private boolean actif;
+	
+	public Emprunt() {
+		
+	}
+
+	public Emprunt(Integer id, Exemplaire exemplaire, User emprunteur, Date debut, Date fin, boolean prolongation,
+			boolean actif) {
+		super();
+		this.id = id;
+		this.exemplaire = exemplaire;
+		this.emprunteur = emprunteur;
+		this.debut = debut;
+		this.fin = fin;
+		this.prolongation = prolongation;
+		this.actif = actif;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Exemplaire getExemplaire() {
+		return exemplaire;
+	}
+
+	public void setExemplaire(Exemplaire exemplaire) {
+		this.exemplaire = exemplaire;
+	}
+
+	public User getEmprunteur() {
+		return emprunteur;
+	}
+
+	public void setEmprunteur(User emprunteur) {
+		this.emprunteur = emprunteur;
+	}
+
+	public Date getDebut() {
+		return debut;
+	}
+
+	public void setDebut(Date debut) {
+		this.debut = debut;
+	}
+
+	public Date getFin() {
+		return fin;
+	}
+
+	public void setFin(Date fin) {
+		this.fin = fin;
+	}
+
+	public boolean isProlongation() {
+		return prolongation;
+	}
+
+	public void setProlongation(boolean prolongation) {
+		this.prolongation = prolongation;
+	}
+
+	public boolean isActif() {
+		return actif;
+	}
+
+	public void setActif(boolean actif) {
+		this.actif = actif;
+	}
+	
+	
 
 }
